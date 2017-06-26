03f4f2ca728d99903195214d48d00bb669251672
Shubham Jain <shubhamkjain@outlook.com>
2017-06-26 18:25:50 +0530

    Refactor code to method and set tgz -file/stream as only supported method

--- dokuwiki-text/src/main/java/org/xwiki/contrib/dokuwiki/text/internal/input/DokuWikiInputFilterStream.java
+++ dokuwiki-text/src/main/java/org/xwiki/contrib/dokuwiki/text/internal/input/DokuWikiInputFilterStream.java
@@ -20,8 +20,10 @@
 package org.xwiki.contrib.dokuwiki.text.internal.input;
 
 import org.apache.commons.compress.archivers.ArchiveEntry;
+import org.apache.commons.compress.archivers.ArchiveException;
 import org.apache.commons.compress.archivers.ArchiveInputStream;
 import org.apache.commons.compress.archivers.ArchiveStreamFactory;
+import org.apache.commons.compress.compressors.CompressorException;
 import org.apache.commons.compress.compressors.CompressorInputStream;
 import org.apache.commons.compress.compressors.CompressorStreamFactory;
 import org.xwiki.component.annotation.Component;
@@ -62,8 +64,17 @@ public class DokuWikiInputFilterStream extends AbstractBeanInputFilterStream<Dok
         InputSource inputSource = this.properties.getSource();
         if (inputSource instanceof FileInputSource)
         {
-            File sourceFolder = ((FileInputSource) inputSource).getFile();
-            readFolder(sourceFolder, filter, proxyFilter);
+            try {
+                CompressorInputStream input = new CompressorStreamFactory()
+                        .createCompressorInputStream(new BufferedInputStream(
+                                ((InputStreamInputSource) inputSource).getInputStream()));
+                assert input != null;
+                ArchiveInputStream archiveInputStream = new ArchiveStreamFactory()
+                        .createArchiveInputStream(new BufferedInputStream(input));
+                readDataStream(archiveInputStream, filter, proxyFilter);
+            } catch (ArchiveException | IOException | CompressorException e) {
+                e.printStackTrace();
+            }
         } else if (inputSource instanceof InputStreamInputSource)
         {
             try {
@@ -71,25 +82,30 @@ public class DokuWikiInputFilterStream extends AbstractBeanInputFilterStream<Dok
                         .createCompressorInputStream(((InputStreamInputSource) inputSource).getInputStream());
                 ArchiveInputStream archiveInputStream = new ArchiveStreamFactory()
                         .createArchiveInputStream(new BufferedInputStream(input));
-                ArchiveEntry archiveEntry = archiveInputStream.getNextEntry();
-                DirectoryTree rootDirectory = new DirectoryTree(new Folder(TAG_ROOT_NODE, TAG_ROOT_NODE));
-                while (archiveEntry != null) {
-                    String entryName = archiveEntry.getName();
-                    rootDirectory.addElement(entryName, archiveEntry.isDirectory());
-                    archiveEntry = archiveInputStream.getNextEntry();
-                }
-                Folder dataDirectory = rootDirectory.getCommonRoot();
-                Folder pagesDirectory = dataDirectory.getChilds()
-                        .get(dataDirectory.getChilds().indexOf(new Folder(TAG_PAGES, TAG_PAGES_PATH)));
-                proxyFilter.beginWikiSpace(TAG_MAIN_SPACE, FilterEventParameters.EMPTY);
-                readPageFolderStream(pagesDirectory, archiveInputStream, filter, proxyFilter);
-                proxyFilter.endWikiSpace(TAG_MAIN_SPACE, FilterEventParameters.EMPTY);
+                readDataStream(archiveInputStream, filter, proxyFilter);
             } catch (Exception e) {
                 throw new FilterException("Unsupported input stream [" + inputSource.getClass() + "]");
             }
         } else {
             throw new FilterException("Unsupported input source [" + inputSource.getClass() + "]");
         }
+    }
+
+    private void readDataStream( ArchiveInputStream archiveInputStream,
+                                 Object filter, DokuWikiFilter proxyFilter) throws IOException, FilterException {
+        DirectoryTree rootDirectory = new DirectoryTree(new Folder(TAG_ROOT_NODE, TAG_ROOT_NODE));
+        ArchiveEntry archiveEntry = archiveInputStream.getNextEntry();
+        while (archiveEntry != null) {
+            String entryName = archiveEntry.getName();
+            rootDirectory.addElement(entryName, archiveEntry.isDirectory());
+            archiveEntry = archiveInputStream.getNextEntry();
+        }
+        Folder dataDirectory = rootDirectory.getCommonRoot();
+        Folder pagesDirectory = dataDirectory.getChilds()
+                .get(dataDirectory.getChilds().indexOf(new Folder(TAG_PAGES, TAG_PAGES_PATH)));
+        proxyFilter.beginWikiSpace(TAG_MAIN_SPACE, FilterEventParameters.EMPTY);
+        readPageFolderStream(pagesDirectory, archiveInputStream, filter, proxyFilter);
+        proxyFilter.endWikiSpace(TAG_MAIN_SPACE, FilterEventParameters.EMPTY);
     }
 
     private void readPageFolderStream(Folder pagesFolderTree, ArchiveInputStream archiveInputStream,
@@ -106,38 +122,38 @@ public class DokuWikiInputFilterStream extends AbstractBeanInputFilterStream<Dok
         }
     }
 
-    private void readFolder(File sourceFolder, Object filter, DokuWikiFilter proxyFilter) throws FilterException {
-        if (sourceFolder.isDirectory()) {
-            File pageFolder = new File(sourceFolder, TAG_PAGES);
-            if (pageFolder.exists() && pageFolder.isDirectory()) {
-                proxyFilter.beginWikiSpace(TAG_MAIN_SPACE, FilterEventParameters.EMPTY);
-                readPagesFolder(pageFolder, filter, proxyFilter);
-                proxyFilter.endWikiSpace(TAG_MAIN_SPACE, FilterEventParameters.EMPTY);
-            } else {
-                throw new FilterException("Can't locate Pages folder: Invalid Package");
-            }
-        } else {
-            throw new FilterException("Input folder is not a directory");
-        }
-    }
+//    private void readFolder(File sourceFolder, Object filter, DokuWikiFilter proxyFilter) throws FilterException {
+//        if (sourceFolder.isDirectory()) {
+//            File pageFolder = new File(sourceFolder, TAG_PAGES);
+//            if (pageFolder.exists() && pageFolder.isDirectory()) {
+//                proxyFilter.beginWikiSpace(TAG_MAIN_SPACE, FilterEventParameters.EMPTY);
+//                readPagesFolder(pageFolder, filter, proxyFilter);
+//                proxyFilter.endWikiSpace(TAG_MAIN_SPACE, FilterEventParameters.EMPTY);
+//            } else {
+//                throw new FilterException("Can't locate Pages folder: Invalid Package");
+//            }
+//        } else {
+//            throw new FilterException("Input folder is not a directory");
+//        }
+//    }
 
-    private void readPagesFolder(File pages, Object filter, DokuWikiFilter proxyFilter) throws FilterException {
-        File[] files = pages.listFiles();
-        if (files != null) {
-            for (File file : files) {
-                if (file.isDirectory()) {
-                    String folder = file.getName();
-                    proxyFilter.beginWikiSpace(folder, FilterEventParameters.EMPTY);
-                    readPagesFolder(file, filter, proxyFilter);
-                    proxyFilter.endWikiSpace(folder, FilterEventParameters.EMPTY);
-                } else if (file.isFile() && file.getName().endsWith(TAG_TEXT_FILE_FORMAT)) {
-                    String fileName = file.getName().replace(TAG_TEXT_FILE_FORMAT, "");
-                    proxyFilter.beginWikiDocument(fileName, FilterEventParameters.EMPTY);
-                    proxyFilter.endWikiDocument(fileName, FilterEventParameters.EMPTY);
-                }
-            }
-        }
-    }
+//    private void readPagesFolder(File pages, Object filter, DokuWikiFilter proxyFilter) throws FilterException {
+//        File[] files = pages.listFiles();
+//        if (files != null) {
+//            for (File file : files) {
+//                if (file.isDirectory()) {
+//                    String folder = file.getName();
+//                    proxyFilter.beginWikiSpace(folder, FilterEventParameters.EMPTY);
+//                    readPagesFolder(file, filter, proxyFilter);
+//                    proxyFilter.endWikiSpace(folder, FilterEventParameters.EMPTY);
+//                } else if (file.isFile() && file.getName().endsWith(TAG_TEXT_FILE_FORMAT)) {
+//                    String fileName = file.getName().replace(TAG_TEXT_FILE_FORMAT, "");
+//                    proxyFilter.beginWikiDocument(fileName, FilterEventParameters.EMPTY);
+//                    proxyFilter.endWikiDocument(fileName, FilterEventParameters.EMPTY);
+//                }
+//            }
+//        }
+//    }
 
     @Override
     public void close() throws IOException {
