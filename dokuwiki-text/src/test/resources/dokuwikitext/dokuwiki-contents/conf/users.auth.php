7bfaca4c5baaaf617c7f55f40a9fe97a60c50829
Neha Gupta <gneha21@yahoo.in>
2018-05-27 00:25:17 +0530

    DOKUWIKI-10 : Support for Directory as a source input. * Adding support for dokuwiki directory as a source input. * Test case added. (dokuwiki-support-folder.test).

---
+++ dokuwiki-text/src/test/resources/dokuwikitext/dokuwiki-contents/conf/users.auth.php
@@ -0,0 +1,14 @@
+# users.auth.php
+# <?php exit()?>
+# Don't modify the lines above
+#
+# Userfile
+#
+# Format:
+#
+# login:passwordhash:Real Name:email:groups,comma,seperated
+
+
+slayerjain:$1$vW08pnm6$p1sSSpZLuwHGW98VCcEHi0:Shubham Jain:shubhamkjain@outlook.com:admin,user
+demouser:$1$p46yTcq0$T2UY9kVww61XRjOaapNd31:Demo User:demo.user@domain.com:demo_group
+ani:$1$ur9JSiGG$6pEp9bkQotYtCSbMz77gD1:Anirudh Jain:jainshubham78@gmail.com:demo_group
